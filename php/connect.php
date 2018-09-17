77edc2030e4009af12c7ff47375ba73cd9d5a2f4
pungpoo <pungpoo23@gmail.com>
2018-09-17 08:56:23 +0700

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php 
-    $conn = new mysqli("localhost","root","BbilkB414148","db_dev");
+    $conn = new mysqli("localhost","root","xxx","db_dev");
     $conn->set_charset("utf8");
     if ($conn->connect_errno) {
         die("Connect Error".$conn->connect_error);
